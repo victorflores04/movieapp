aa3f7ed56dceaba64e37fa1039c1dc51f40ae2aa
Victor Flores <flores.vicf@gmail.com>
2020-09-07 10:34:32 -0500

    ESlint

---
+++ src/components/helloword.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import '../assets/styles/App.scss';
+
+const HelloWord =()=>(
+    <h1>prueba de react personalizado</h1>
+);
+    
+
+export default HelloWord;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import HelloWord from './components/helloword';
+
+ReactDOM.render(<HelloWord/>, document.getElementById('app'));
+
