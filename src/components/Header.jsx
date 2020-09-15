c8092fb34e4bfd3fb51cd8e6931f1ab69d93fbae
Victor Flores <flores.vicf@gmail.com>
2020-09-14 21:25:10 -0500

    modificacion header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import '../assets/styles/components/Header.scss';
 import logo from '../assets/static/logo-south-video-BW2.png';
+import user from '../assets/static/user-icon.png';
 const Header = () =>(
   
   <header class="header">
-    <img class="header__img" src={logo} alt="Platzi Video"/>
+    <img class="header__img" src={logo} alt="South Video"/>
     <div class="header__menu--nav">
     <nav>
         <a>Eventos</a>
@@ -15,7 +16,7 @@ const Header = () =>(
       </div>
     <div class="header__menu">
       <div class="header__menu--profile">
-        <img src="../assets/user-icon.png" alt=""/>
+        <img src={user} alt="user-icon"/>
         <p>Perfil</p>
       </div>
       <ul>
